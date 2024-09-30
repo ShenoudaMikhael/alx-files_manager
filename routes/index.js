a3c3d07bfefbc2a18e8c4a24d1893fce71b94807
Salsabil <salsabeelahmed323@gmail.com>
2024-09-30 13:57:11 +0300

    rdit index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
-const router = Router();
+const router = express.Router();
 
 const routeController = (app) => {
   app.use('/', router);
-  router.get('/status', AppController.getStatus);
-  router.get('/stats', AppController.getStats);
-  router.post('/users', UsersController.postNew);
+
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
 };
 
 export default routeController;
