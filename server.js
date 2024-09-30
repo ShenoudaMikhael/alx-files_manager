41a6193c4268a5aebbbf8bf4bc7fb38c5ad6a23d
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-30 20:29:11 +0300

    Fix 6

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 import express from 'express';
-import router from './routes/index.js';
+import router from './routes/index';
 
 const app = express();
 app.use(express.json());
@@ -13,3 +13,4 @@ app.listen(port, () => {
 });
 
 export default app;
+module.exports = app;
