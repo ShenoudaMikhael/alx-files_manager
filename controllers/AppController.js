d2668f3cc72c40ca1a904d85e49620d30402bc93
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-30 20:14:39 +0300

    Fix2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-/* eslint-disable no-unused-vars */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
+  static getStatus(req, res) { /* eslint-disable no-unused-vars */
     res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
     });
   }
 
-  static async getStats(req, res) {
+  static async getStats(req, res) { /* eslint-disable no-unused-vars */
     const users = await dbClient.nbUsers();
     const files = await dbClient.nbFiles();
     res.status(200).json({ users, files });
