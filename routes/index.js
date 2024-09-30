4e4aaa43fe9a5188ae83ba213dd702b2488ce9d8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-30 07:26:51 +0300

    3. Create a new user

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,5 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.get('/users', UsersController.postNew);
+router.post('/users', UsersController.postNew);
 export default router;
