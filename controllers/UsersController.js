99f0042dfc192d449611336fd52f5c2339ff1c65
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-30 07:00:42 +0300

    3. Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,14 +6,14 @@ class UsersController {
   static postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-      res.status(400).send('Missing email');
+      res.status(400).send({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).send('Missing password');
+      res.status(400).send({ error: 'Missing password' });
     }
     const dbUser = dbClient.usersCollection.find({ email });
     if (dbUser) {
-      res.status(400).send('Already exist');
+      res.status(400).send({ error: 'Already exist' });
     }
     const hashedPass = bcrypt.hash(password);
     const newUser = dbClient.usersCollection.insert({ email, password: hashedPass });
