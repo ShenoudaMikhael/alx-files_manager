3b959412d3115089ac4bfb91bee704118d29c8d5
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-09-30 20:52:35 +0300

    Fix 10

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,19 +3,17 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  static getStatus(req, res) { /* eslint-disable no-unused-vars */
-    res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    });
-  }
-
-  static async getStats(req, res) { /* eslint-disable no-unused-vars */
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
-    res.status(200).json({ users, files });
-  }
+function getStatus(req, res) { /* eslint-disable no-unused-vars */
+  res.status(200).json({
+    redis: redisClient.isAlive(),
+    db: dbClient.isAlive(),
+  });
 }
 
-module.exports = AppController;
+async function getStats(req, res) { /* eslint-disable no-unused-vars */
+  const users = await dbClient.nbUsers();
+  const files = await dbClient.nbFiles();
+  res.status(200).json({ users, files });
+}
+
+module.exports = { getStatus, getStats };
