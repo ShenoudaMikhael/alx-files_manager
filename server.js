048c58067563770f7b371ed88cb35bf88f89f924
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-10-03 00:07:35 +0300

    2. First API

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import express from 'express';
 import router from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', router);
+router(app);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
